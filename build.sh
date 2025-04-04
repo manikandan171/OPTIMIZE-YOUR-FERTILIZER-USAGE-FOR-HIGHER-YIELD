c524c31b990bfe95ea5486418b76928c3cbb4615
manikandan <112394806+manikandan171@users.noreply.github.com>
2025-04-04 23:28:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-docker build -t task37 .
-echo Hyyy
-docker login -u mani1711 -p Rithvikmani123#
-docker tag task37 mani1711/finaltask
-docker push mani1711/finaltask
-kubectl apply -f  deploy.yaml --validate=false
-kubectl apply -f svc.yaml --validate=false
+docker build -t test .
+docker run -itd -p 99:80 test
